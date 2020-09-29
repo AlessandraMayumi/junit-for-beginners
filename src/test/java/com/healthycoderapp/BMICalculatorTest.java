9c5aa97627f59bbb6e362373500b379943a7a151
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-09-29 14:24:17 -0300

    first commit

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,42 @@
+package test.java.com.healthycoderapp;
+
+import main.java.com.healthycoderapp.BMICalculator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+    @Test
+    void should_Return_True_When_DietRecommended() {
+        // given
+        double weight = 89.0;
+        double height = 1.72;
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+        // then
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_Return_False_When_DietRecommended() {
+        // given
+        double weight = 50.0;
+        double height = 1.92;
+        // when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+        // then
+        assertFalse(recommended);
+    }
+
+    @Test
+    void should_ThrowArithmeticException_When_DietRecommended() {
+        // given
+        double weight = 50.0;
+        double height = 0.0;
+        // when
+        Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+        // then
+        assertThrows(ArithmeticException.class, executable);
+    }
+}
